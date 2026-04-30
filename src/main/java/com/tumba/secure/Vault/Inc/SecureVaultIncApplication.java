9dda655ae5fff00538286416894e75bf8148e639
system sylvere BARINDA <systembarinda@gmail.com>
2026-04-30 11:45:16 +0200

    initial set

---
+++ src/main/java/com/tumba/secure/Vault/Inc/SecureVaultIncApplication.java
@@ -0,0 +1,13 @@
+package com.tumba.secure.Vault.Inc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVaultIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureVaultIncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tumba/secure/Vault/Inc/SecureVaultIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tumba.secure.Vault.Inc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
